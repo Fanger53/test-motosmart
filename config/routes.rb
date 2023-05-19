c6f237d4b341a1965f998936577d8937bc9fe34f
Fanger53 <fanger53@gmail.com>
2023-05-19 18:31:11 -0500

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      root 'rankings#by_month_year'
+      root 'rankings#current'
       get 'ranking/actual', to: 'rankings#current'
       get 'ranking/:year/:month', to: 'rankings#by_month_year'
       post 'ranking/record', to: 'rankings#record'
