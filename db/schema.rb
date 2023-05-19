ea3e6dcb75449b1c40c2c36cd318617d00088f62
Fanger53 <fanger53@gmail.com>
2023-05-19 10:32:20 -0500

    adding Ranking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_151654) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_152348) do
+  create_table "rankings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "kilometers"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rankings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_151654) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "rankings", "users"
 end
