8b2c78aadd29bc9ceb986f86a6435295d7edee26
Fanger53 <fanger53@gmail.com>
2023-05-19 18:16:04 -0500

    model user with auntentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  require 'securerandom'
+  before_create :generate_authentication_token
   has_many :rankings
+
+  private
+
+  def generate_authentication_token
+    self.uuid = SecureRandom.uuid
+    self.token = SecureRandom.hex(32)
+  end
 end
